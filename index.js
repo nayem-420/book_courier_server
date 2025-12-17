09929176ddbc4757c3d51a57f038e112674559d2
nayem181 <nayemimranhasan@gmail.com>
2025-12-17 12:13:02 +0600

    add the middleware

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
+
+
+// middleware
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Book Courier Server is Running on port!");
