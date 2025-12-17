a5f8ecedd368c86caf92e0cfa638f09c7e1eb2d9
nayem181 <nayemimranhasan@gmail.com>
2025-12-17 13:24:40 +0600

    setting the secrect password from mongodb

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors());
 
 
-const uri = `mongodb+srv://<db_username>:<db_password>@cluster0.xhgpsyg.mongodb.net/?appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xhgpsyg.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -32,7 +32,7 @@ async function run() {
       
 
 
-      
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
