bf3e1d6f68803e4e260754e8375d9ccb9154b2e1
nayem181 <nayemimranhasan@gmail.com>
2025-12-17 14:50:29 +0600

    add the get request for the books

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
-
 const port = process.env.PORT || 3000;
-
 
 // middleware
 app.use(express.json());
 app.use(cors());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xhgpsyg.mongodb.net/?appName=Cluster0`;
 
@@ -21,31 +18,35 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
-
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-      
+    await client.connect();
 
-      const db = client.db('book-courier-db');
-      const booksCollection = db.collection('books');
+    const db = client.db("book-courier-db");
+    const booksCollection = db.collection("books");
 
+    //   books related API's
+    app.get("/books", async (req, res) => {
+      const query = {};
+      const { email } = req.query;
 
-      //   books related API's
-      app.get('/books', async(req, res) = {
-          
-      })
-      app.post('/books', async (req, res) => {
-          const books = req.body;
-          const result = await booksCollection.insertOne(books);
-          res.send(result);
-      })
+      if (email) {
+        query.email = email;
+      }
+      const cursor = booksCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-
+    app.post("/books", async (req, res) => {
+      const books = req.body;
+      const result = await booksCollection.insertOne(books);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
